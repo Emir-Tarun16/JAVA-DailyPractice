36d733f7c2dab59c56e15a70c11d79da8096ec05
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-04-20 13:22:06 +0530

    day 2 grunt

---
+++ day2.java
@@ -0,0 +1,12 @@
+// Writing very first java program without using the semicoloumn..
+
+import java.lang.*;
+
+public class day2 {
+	public static void main(String []args){
+		
+		if(System.out.printf("Hello World") == null){
+		}
+		
+	}	
+}
