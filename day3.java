1c109ef371bef04f272a7cdb1aaebaea4ce3f9cb
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-04-21 22:56:36 +0530

    day 3 Done

---
+++ day3.java
@@ -0,0 +1,15 @@
+// Day 3 Challenge 
+
+import java.lang.*;
+public class day3 {
+	
+	public static void main(String []args) {
+		System.out.println("Fill the 500ml Water Fully ");
+		int firstCup = 500;
+		System.out.println("Fill the 500ml Cup Water into 300ml upto full ");
+		int secondCup = 300;
+		
+		int finalCup = firstCup - secondCup;
+		System.out.println("Hence , Chef will water exactly that is : "+finalCup+"ml");
+	}
+}
